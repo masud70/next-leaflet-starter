d74fa6466343f6df8312ebaaf5a928c61c182a9c
Md. Masud Mazumder <mdmasud.csecu@gmail.com>
2022-12-19 13:54:30 +0600

    Final Project Structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,55 +1,89 @@
-import Head from 'next/head';
+import Head from "next/head";
 
-import Layout from '@components/Layout';
-import Section from '@components/Section';
-import Container from '@components/Container';
-import Map from '@components/Map';
-import Button from '@components/Button';
+import Layout from "@components/Layout";
+import Section from "@components/Section";
+import Container from "@components/Container";
+import Map from "@components/Map";
+import Button from "@components/Button";
 
-import styles from '@styles/Home.module.scss';
+import styles from "@styles/Home.module.scss";
+import { useEffect, useState } from "react";
 
-const DEFAULT_CENTER = [38.907132, -77.036546]
+const DEFAULT_CENTER = [23.729211164246585, 90.40874895549243];
+const p2 = [38.90724, -77.036545];
 
 export default function Home() {
-  return (
-    <Layout>
-      <Head>
-        <title>Next.js Leaflet Starter</title>
-        <meta name="description" content="Create mapping apps with Next.js Leaflet Starter" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Section>
-        <Container>
-          <h1 className={styles.title}>
-            Next.js Leaflet Starter
-          </h1>
-
-          <Map className={styles.homeMap} width="800" height="400" center={DEFAULT_CENTER} zoom={12}>
-            {({ TileLayer, Marker, Popup }) => (
-              <>
-                <TileLayer
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                  attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+    const [data, setData] = useState({});
+    const fetchData = async () => {
+        const req = await fetch("api/get_data");
+        const data = await req.json();
+        setData(data);
+    };
+    useEffect(() => {
+        fetchData();
+    }, []);
+    return (
+        <Layout>
+            <Head>
+                <title>Next.js Leaflet Starter</title>
+                <meta
+                    name="description"
+                    content="Create mapping apps with Next.js Leaflet Starter"
                 />
-                <Marker position={DEFAULT_CENTER}>
-                  <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                  </Popup>
-                </Marker>
-              </>
-            )}
-          </Map>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-          <p className={styles.description}>
-            <code className={styles.code}>yarn create next-app -e https://github.com/colbyfayock/next-leaflet-starter</code>
-          </p>
+            <Section>
+                <Container>
+                    <h1 className={styles.title}>Next.js Leaflet Starter</h1>
 
-          <p className={styles.view}>
-            <Button href="https://github.com/colbyfayock/next-leaflet-starter">Vew on GitHub</Button>
-          </p>
-        </Container>
-      </Section>
-    </Layout>
-  )
+                    <Map
+                        className={styles.homeMap}
+                        width="800"
+                        height="400"
+                        center={DEFAULT_CENTER}
+                        zoom={12}
+                    >
+                        {({ TileLayer, Marker, Popup }) => (
+                            <>
+                                <TileLayer
+                                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                                />
+                                {data.map((item, idx) =>
+                                    item.location_coordinates.map((pos, id) => (
+                                        <Marker
+                                            position={[pos.lat, pos.long]}
+                                            key={`project_${idx}_marker_${
+                                                id + 1
+                                            }`}
+                                        >
+                                            <Popup>
+                                                Project Name:{" "}
+                                                {item.project_name} <br />{" "}
+                                                Easily customizable.
+                                            </Popup>
+                                        </Marker>
+                                    ))
+                                )}
+                            </>
+                        )}
+                    </Map>
+
+                    <p className={styles.description}>
+                        <code className={styles.code}>
+                            yarn create next-app -e
+                            https://github.com/colbyfayock/next-leaflet-starter
+                        </code>
+                    </p>
+
+                    <p className={styles.view}>
+                        <Button href="https://github.com/colbyfayock/next-leaflet-starter">
+                            Vew on GitHub
+                        </Button>
+                    </p>
+                </Container>
+            </Section>
+        </Layout>
+    );
 }
